c7d411ee4b2ce662f2334a55aef30222b2249b56
RabaDaba1 <krabczewski@gmail.com>
2023-03-23 00:41:02 +0100

    Create first test

--- example_02_solvable.py
+++ example_02_solvable.py
@@ -1,26 +1,47 @@
 import logging
-from saport.simplex.model import Model 
+from saport.simplex.model import Model
+
 
 def create_model() -> Model:
     model = Model(__file__)
 
-    #TODO:
+    # TODO:
     # fill missing test based on the example_01_solvable.py
     # to make the test a bit more interesting:
     # * minimize the objective (so the solver would have to normalize it)
     # * make some ">=" constraints (GE)
     # * the model still has to be solvable by the basix simplex withour artificial var
 
-    return model 
+    x1 = model.create_variable("x1")
+    x2 = model.create_variable("x2")
+    x3 = model.create_variable("x3")
+
+    model.add_constraint(x1 + x2 + x3 <= 30)
+    model.add_constraint(x1 + 2 * x2 + x3 >= 10)
+    model.add_constraint(2 * x2 + x3 <= 20)
+
+    model.maximize(2 * x1 + x2 + 3 * x3)
+
+    return model
+
 
 def run():
     model = create_model()
-    #TODO:
+    # TODO:
     # add a test "assert something" based on the example_01_solvable.py
     # TIP: you may use other solvers (e.g. https://online-optimizer.appspot.com)
     #      to find the correct solution
-    logging.info("This test is empty but it shouldn't be, fix it!")
-    raise AssertionError("Test is empty")
+    try:
+        solution = model.solve()
+    except:
+        raise AssertionError("This problem has a solution and your algorithm hasn't found it!")
+
+    logging.info(solution)
+
+    assert (solution.assignment(model) == [10, 0, 20]), "Your algorithm found an incorrect solution!"
+
+    logging.info("Congratulations! This solution seems to be alright :)")
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(message)s')
